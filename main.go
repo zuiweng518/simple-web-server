80b65c4b737310d54d4af933f6383f166bfd4b0f
shenghao <mashenghao@yahoo.cn>
2023-03-15 14:44:56 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
   "github.com/labstack/echo/v4"
   "github.com/labstack/echo/v4/middleware"
   "net/http"
+  "os"
+  "bufio"
 )
 
 // some change - 2
@@ -21,10 +23,24 @@ func main() {
 
   // Start server
   e.Logger.Fatal(e.Start(":1325"))
+  os.Getppid()
 }
 
 // Handler
 func hello(c echo.Context) error {
   return c.String(http.StatusOK, "Hello, World! -- 17")
 }
-
+func  CreatePidFile(){
+    filePath := "/opt/simple-web/pid"
+    file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0666)
+    if err != nil {
+        fmt.Println("文件打开失败", err)
+    }
+    //及时关闭file句柄
+    defer file.Close()
+    //写入文件时，使用带缓存的 *Writer
+    write := bufio.NewWriter(file)
+    write.WriteString(os.Getppid())
+    //Flush将缓存的文件真正写入到文件中
+    write.Flush()
+}
